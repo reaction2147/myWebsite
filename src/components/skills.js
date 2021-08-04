f552d7a9064e28ae979a1b510660b46a030e86cb
William Ford <wford1.home@gmail.com>
2021-08-04 21:54:05 +0100

    Added skill section component

---
+++ src/components/skills.js
@@ -0,0 +1,158 @@
+import React from "react";
+import "../css/home.css";
+import icon from "../icons/icon_html.svg";
+
+const Skills = () => {
+  return (
+    <>
+      <section className="section">
+          <div className="title-container">
+        <h1 className="title is-3 is-spaced space-right">Tools &amp; Skills</h1>
+        <div className="title-line"></div>
+        </div>
+        <div className="columns is-mobile is-centered">
+          <div className="column">
+            <h1 className="subtitle is-5 has-text-centered">Back End</h1>
+            <div className="box has-background-grey-dark ">
+              <div className="tile is-ancestor">
+                <div className="tile is-parent is-4">
+                  <div className="tile is-child has-text-centered">
+                    <img className="is-inline-block" src={icon} />
+                    <p className="has-text-white">HMTL</p>
+                  </div>
+                </div>
+                <div className="tile is-parent is-4">
+                  <div className="tile is-child has-text-centered">
+                    <img className="is-inline-block" src={icon} />
+                    <p className="has-text-white">HMTL</p>
+                  </div>
+                </div>
+                <div className="tile is-parent is-4">
+                  <div className="tile is-child has-text-centered">
+                    <img className="is-inline-block" src={icon} />
+                    <p className="has-text-white">HMTL</p>
+                  </div>
+                </div>
+                </div>
+                <div className="tile is-ancestor">
+                <div className="tile is-parent">
+                  <div className="tile is-child has-text-centered">
+                    <img className="is-inline-block" src={icon} />
+                    <p className="has-text-white">HMTL</p>
+                  </div>
+                </div>
+                <div className="tile is-parent">
+                  <div className="tile is-child has-text-centered">
+                    <img className="is-inline-block" src={icon} />
+                    <p className="has-text-white">HMTL</p>
+                  </div>
+                </div>
+                <div className="tile is-parent">
+                  <div className="tile is-child has-text-centered">
+                    <img className="is-inline-block" src={icon} />
+                    <p className="has-text-white">HMTL</p>
+                  </div>
+                </div>
+              </div>
+              </div>
+            </div>
+          
+            <div className="column">
+            <h1 className="subtitle is-5 has-text-centered">Back End</h1>
+            <div className="box has-background-grey-dark ">
+              <div className="tile is-ancestor">
+                <div className="tile is-parent is-4">
+                  <div className="tile is-child has-text-centered">
+                    <img className="is-inline-block" src={icon} />
+                    <p className="has-text-white">HMTL</p>
+                  </div>
+                </div>
+                <div className="tile is-parent is-4">
+                  <div className="tile is-child has-text-centered">
+                    <img className="is-inline-block" src={icon} />
+                    <p className="has-text-white">HMTL</p>
+                  </div>
+                </div>
+                <div className="tile is-parent is-4">
+                  <div className="tile is-child has-text-centered">
+                    <img className="is-inline-block" src={icon} />
+                    <p className="has-text-white">HMTL</p>
+                  </div>
+                </div>
+                </div>
+                <div className="tile is-ancestor">
+                <div className="tile is-parent">
+                  <div className="tile is-child has-text-centered">
+                    <img className="is-inline-block" src={icon} />
+                    <p className="has-text-white">HMTL</p>
+                  </div>
+                </div>
+                <div className="tile is-parent">
+                  <div className="tile is-child has-text-centered">
+                    <img className="is-inline-block" src={icon} />
+                    <p className="has-text-white">HMTL</p>
+                  </div>
+                </div>
+                <div className="tile is-parent">
+                  <div className="tile is-child has-text-centered">
+                    <img className="is-inline-block" src={icon} />
+                    <p className="has-text-white">HMTL</p>
+                  </div>
+                </div>
+              </div>
+              </div>
+            </div>
+
+            <div className="column">
+            <h1 className="subtitle is-5 has-text-centered">Back End</h1>
+            <div className="box has-background-grey-dark ">
+              <div className="tile is-ancestor">
+                <div className="tile is-parent is-4">
+                  <div className="tile is-child has-text-centered">
+                    <img className="is-inline-block" src={icon} />
+                    <p className="has-text-white">HMTL</p>
+                  </div>
+                </div>
+                <div className="tile is-parent is-4">
+                  <div className="tile is-child has-text-centered">
+                    <img className="is-inline-block" src={icon} />
+                    <p className="has-text-white">HMTL</p>
+                  </div>
+                </div>
+                <div className="tile is-parent is-4">
+                  <div className="tile is-child has-text-centered">
+                    <img className="is-inline-block" src={icon} />
+                    <p className="has-text-white">HMTL</p>
+                  </div>
+                </div>
+                </div>
+                <div className="tile is-ancestor">
+                <div className="tile is-parent">
+                  <div className="tile is-child has-text-centered">
+                    <img className="is-inline-block" src={icon} />
+                    <p className="has-text-white">HMTL</p>
+                  </div>
+                </div>
+                <div className="tile is-parent">
+                  <div className="tile is-child has-text-centered">
+                    <img className="is-inline-block" src={icon} />
+                    <p className="has-text-white">HMTL</p>
+                  </div>
+                </div>
+                <div className="tile is-parent">
+                  <div className="tile is-child has-text-centered">
+                    <img className="is-inline-block" src={icon} />
+                    <p className="has-text-white">HMTL</p>
+                  </div>
+                </div>
+              </div>
+              </div>
+            </div>
+
+          </div>
+      </section>
+    </>
+  );
+};
+
+export default Skills;
